8c5f1f1fe0a6762101e4b5f7312f4d3ccdc4aa34
gholib40 <ghoelibsoekamti@gmail.com>
2024-05-10 18:05:13 +0700

    weqeqeqeqe

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,19 @@
+import { createSelector } from "reselect"
 
 
-export const selectCategoriesMap = (state) => state.categories.categories
-.reduce((acc,category) => {
+const selectCategoryReducer = (state)=> state.categories
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice)=> categoriesSlice.categories
+    
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategoryReducer],
+    (categories) => categories.reduce((acc,category) => {
     const {title,items} = category
     acc[title.toLowerCase()] = items
     return acc
     },{})
+)
